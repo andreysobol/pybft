594b2d7c19285a25fb5f60ddc63bb659c70d3fbc
Andrey Sobol <andreysobol@protonmail.com>
2022-08-11 19:29:07 +0200

    commit,     parse_title,     parse_request,     pre_prepare,     parse_pre_prepare,     prepare,     parse_prepare,     commit,     parse_commit

---
+++ messages.py
@@ -0,0 +1,102 @@
+from state_machine import StateMachineOperation
+
+def serialize_title(data):
+    first = data.encode('utf-8')
+    increase = 16 - len(first)
+    second = b'\x00' * increase
+    return first + second 
+
+def deserialize_title(blob):
+    pre =  blob[:16].decode('utf-8')
+    return pre.split('\x00')[0]
+
+def parse_title(message):
+    return deserialize_title(message[:16])
+
+# o - state machine operation or state transition in other word
+# t - timestamp
+# c - client id
+def request(o, t, c):
+    title = serialize_title("request")
+    return title + o.serialize() + t.to_bytes(8, byteorder='big') + c.to_bytes(8, byteorder='big')
+
+def parse_request(message) -> tuple:
+    return (
+        StateMachineOperation.deserialize(message[16:20]),
+        int.from_bytes(message[20:28], byteorder='big'),
+        int.from_bytes(message[28:36], byteorder='big'),
+    )
+
+# v - view number
+# n - sequence number
+# d - digest for message m
+# signature
+# m - message, request
+def pre_prepare(v, n, d, signature, m):
+    title = serialize_title("pre_prepare")
+    sv = v.to_bytes(8, byteorder='big')
+    sn = n.to_bytes(8, byteorder='big')
+    sd = d
+    # 64 bytes for signature
+    ssignature = signature
+    sm = m
+    return title + sv + sn + sd + ssignature + sm
+
+def parse_pre_prepare(message) -> tuple:
+    return (
+        int.from_bytes(message[16:24], byteorder='big'),
+        int.from_bytes(message[24:32], byteorder='big'),
+        message[32:64],
+        message[64:128],
+        parse_request(message[48:])
+    )
+
+
+# v - view number
+# n - sequence number
+# d - digest for message m
+# i - replica id
+# signature
+def prepare(v, n, d, i, signature):
+    title = serialize_title("prepare")
+    sv = v.to_bytes(8, byteorder='big')
+    sn = n.to_bytes(8, byteorder='big')
+    sd = d
+    si = i.to_bytes(8, byteorder='big')
+    # 64 bytes for signature
+    ssignature = signature
+    return title + sv + sn + sd + si + ssignature
+
+def parse_prepare(message) -> tuple:
+    return (
+        int.from_bytes(message[16:24], byteorder='big'),
+        int.from_bytes(message[24:32], byteorder='big'),
+        message[32:64],
+        int.from_bytes(message[64:72], byteorder='big'),
+        message[72:136],
+    )
+
+
+# v - view number
+# n - sequence number
+# d - digest for message m
+# i - replica id
+# signature
+def commit(v, n, d, i, signature):
+    title = serialize_title("prepare")
+    sv = v.to_bytes(8, byteorder='big')
+    sn = n.to_bytes(8, byteorder='big')
+    sd = d
+    si = i.to_bytes(8, byteorder='big')
+    # 64 bytes for signature
+    ssignature = signature
+    return title + sv + sn + sd + si + ssignature
+
+def parse_commit(message) -> tuple:
+    return (
+        int.from_bytes(message[16:24], byteorder='big'),
+        int.from_bytes(message[24:32], byteorder='big'),
+        message[32:64],
+        int.from_bytes(message[64:72], byteorder='big'),
+        message[72:136],
+    )
